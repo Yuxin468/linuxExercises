7723fa920b626af630c089829f70cb842215265a
jren76 <115088208+jren76@users.noreply.github.com>
2022-10-20 23:09:43 -0500

    Update

--- mean.sh
+++ mean.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-
+                                                                        
 if [[ $# != [12] ]]
 then
     echo "usage: $0 <column> [file.csv]" 1>&2
     exit 0
 fi
 
+column=$1
+
 if [[ $# == 1 ]]
 then
-    column=$1
     file=/dev/stdin
 else
-    column=$1
     file=$2
 fi
 
@@ -20,9 +20,8 @@ cut -d, -f$column < $file | tail -n +2 |{
     sum=0
     while read n
     do
-      	count=$(($count + 1))
+	count=$(($count + 1))
 	sum=$(echo "$sum + $n" | bc)
     done
     mean=$(echo "$sum / $count"| bc -l)
-    echo "$mean"
-}
+    echo "$mean"}
