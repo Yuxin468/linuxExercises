5cb507ca23e11e8bfe6b29db3bf9fa6475653bcb
jren76 <115088208+jren76@users.noreply.github.com>
2022-10-21 20:21:43 -0500

    Final

--- rm_n.sh
+++ rm_n.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ $# -ne 2]; then
+if [ $# -ne 2 ]; then
     echo "usage: $0 <dir> <n>" 1>&2
     exit 0
 fi
 cd "$1"
 file=$(find . -type f -size +$2c)
-for f in $file; do
-    rm $f
+for i in $file; do
+    rm $i
 done
 
