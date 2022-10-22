17bc4066fb39d30ba2d3ce8bdc2a2be92f084b45
jren76 <115088208+jren76@users.noreply.github.com>
2022-10-21 19:28:46 -0500

    Update

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,18 +1,18 @@
-function five_dirs {
-	mkdir five
-	for i in 1 2 3 4 5;
+#!/bin/bash
+
+mkdir five
+for i in 1 2 3 4 5;
+do
+    mkdir five/dir$i	
+    for dir in five/dir$i
+    do
+	for j in 1 2 3 4
 	do
-	mkdir five/dir$i	
-	  for dir in five/dir$i
-	  do
-	  for j in 1 2 3 4;
-	  do
 	    touch $dir/file$j
-	    for t in $(seq $j);
+	    for t in $(seq $j)
 	    do
-	    echo "$j" >> $dir/file$j
+		echo "$j" >> $dir/file$j
 	    done
-	  done
-	  done
-	 done
-}
+	done
+    done
+done
