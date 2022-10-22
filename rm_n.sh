0717c9ff5c861146953b2ae34e096a9d00da9cdb
YulingLyu <ylyu68@wisc.edu>
2022-10-21 20:06:12 -0500

    Final

--- rm_n.sh
+++ rm_n.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-dir=$1
-n=$2
+
 if [ $# -ne 2]; then
     echo "usage: $0 <dir> <n>" 1>&2
     exit 0
 fi
-for i in $(find $dir -type f -size $n); do
-    rm "$i"
-    done
+cd "$1"
+file=$(find . -type f -size +$2c)
+for i in $file; do
+    rm $i
+done
+
